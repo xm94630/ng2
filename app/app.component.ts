8c17c2a03d973a1e3337d1ca64ef1da085380f81
xm94630 <xm94630@126.com>
2016-10-08 08:59:44 +0800

    add

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <div class="jumbotron">
-      <h1>Hi,xm94630!</h1>
-    </div>
+    <header>
+		<nav class="navbar navbar-inverse">
+			<a href="" class="navbar-brand">My Angular 2 App</a>
+		</nav>
+    </header>
+    
+    <main>
+		<div class="jumbotron">
+		  <h1>Hi,xm94630!</h1>
+		  <h1>{{message}}</h1>
+		</div>
+		
+		<div *ngFor="let user of users">
+			<p>用户名字是{{user.name}}（id:{{user.id}}）</p>
+		</div>
+	</main>
+
+    <footer class="text-center">
+	Copyright &copy; 2016
+    </footer>
   `,
   styles: [`
     .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
   `]
 })
-export class AppComponent {}
+export class AppComponent {
+	message = 'Hello';
+	users = [
+		{id:123,name:'浙小孩',username:'浙小孩'},
+		{id:234,name:'张三',username:'张三'},
+		{id:345,name:'李四',username:'李四'}
+	];
+}
